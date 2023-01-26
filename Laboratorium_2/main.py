04f355cdda98608f0d65a33783d67f51e5cc8964
Fluntin <112729558+Fluntin@users.noreply.github.com>
2023-01-26 14:03:08 +0100

    Final

--- Laboratorium_2/main.py
+++ Laboratorium_2/main.py
@@ -1,5 +1,5 @@
 import sys
-#from ArrayQ import ArrayQ
+#from arrayQFile import ArrayQ
 from linkedQFile import LinkedQ
 
 if __name__ == "__main__":
@@ -7,18 +7,13 @@ if __name__ == "__main__":
     deck=LinkedQ()
     
     indata = sys.stdin.readline()
-    #indata=input()
     indata=indata.split()
-    #print("\n")
-    #print(indata)
-    
+
     
     for i in range(len(indata)):
-    #while deck.size()<5:
         deck.enqueue((indata[i]))
 
     count=0
-    svar=list()
     while deck.isEmpty() == False:
 
         card=deck.dequeue()
@@ -27,8 +22,4 @@ if __name__ == "__main__":
         if count%2 !=0:
             deck.enqueue(card)
         else:
-            svar.append(card)
-            
-    
-    for element in svar:
-        print(element, end =" ")
+            print(card, end =" ")
