1671884c8610c8c6eafaff8da7395e0fe08fd07a
Fluntin <112729558+Fluntin@users.noreply.github.com>
2023-03-19 19:17:24 +0100

    Not working

---
+++ Laboratorium_8/Syntaxfel.py
@@ -0,0 +1,125 @@
+
+#En enkel molekyl kan beskrivas med ett atomnamn, eventuellt följt av antalet atomer, till exempel Xe, H2 eller Cr12.
+#
+#      <molekyl> ::= <atom> | <atom><num>
+#      <atom>  ::= <LETTER> | <LETTER><letter>
+#      <LETTER>::= A | B | C | ... | Z
+#      <letter>::= a | b | c | ... | z
+#      <num>   ::= 2 | 3 | 4 | ...
+#
+#För varje inmatad rad ska programmet skriva ut ett omdöme,
+#
+# "Formeln är syntaktiskt korrekt"
+# "Saknad stor bokstav vid radslutet"
+# "För litet tal vid radslutet"  
+# 
+# följt av en utskrift av den del av inmatningen som är kvar efter det tecken där felet påträffades.
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
+#Skriv funktionshuvud för fem funktioner, en för varje regel i syntaxen ovan. Funktionskroppen ska fyllas i senare, i punkt 5.
+
+#Gör en kopia av din LinkedQueue från labb 2, och lägg till metoden peek() som tittar på nästa värde i kön utan att plocka ut det.
+
+#Gör ett eget särfall Syntaxfel som är subklass till (ärver från) Exception.
+
+#Skriv ett testprogram med unittest som ska kontrollera att dina funktioner fungerar som avsett. Se exempel från syntaxföreläsningen. 
+#T ex kan ett test vara att kön innehåller en syntaktiskt korrekt molekyl, som A -> a -> 5 (OBS! De fem funktionerna ska skrivas i nästa punkt.)
+
+#Lägg till kod i de fem funktionerna som kontrollerar syntaxen. 
+# Om en funktion upptäcker ett fel, tex Saknad stor bokstav eller För litet tal vid radslutet ska den göra raise Syntaxfel(felmeddelande). 
+# Om allt gått bra gör funktionen inget.
+
+#Provkör med ditt testprogram.
+
+#-----------------------------------------------------------------------------------------------------------------------------
+from linkedQueue import LinkedQ
+import string
+
+#First, we define the Syntaxfel exception class that we will use for raising syntax errors during molecule parsing:
+class Syntaxfel(Exception):
+    pass
+
+#Next, we define the five functions that will check the syntax of a molecule according to the provided rules. We will implement each rule as a function.
+
+#-----------------------------------------------------------------------------------------------------------------------------
+#Rule 1: <molecule> ::= <atom> | <atom><num>
+
+def readMolecule(q):
+    readAtom(q)
+    if q.peek() == None:
+        return
+    elif q.peek() == ".":
+        q.dequeue()
+    else:
+        readNumber(q)
+        #readMolecule(q)
+
+
+#-----------------------------------------------------------------------------------------------------------------------------
+#Rule 2: <atom> ::= <LETTER> | <LETTER><letter>
+def readAtom(q):
+    readLETTER(q)
+    if q.peek() is not None and q.peek() in list(string.ascii_lowercase):
+        readletter(q)
+    return
+
+
+#-----------------------------------------------------------------------------------------------------------------------------       
+#Rule 3: <LETTER>::= A | B | C | ... | Z
+def readLETTER(q):
+    if q.peek() is None:
+        raise Syntaxfel("Saknad stor bokstav")
+    letter = q.dequeue()
+    if letter in string.ascii_letters:
+        return letter
+    raise Syntaxfel("Saknad stor bokstav")
+    
+
+
+#-----------------------------------------------------------------------------------------------------------------------------
+#Rule 4: <letter>::= a | b | c | ... | z
+def readletter(q):
+    letter = q.dequeue()
+    return
+    
+#-----------------------------------------------------------------------------------------------------------------------------
+#Rule 5: <num> ::= 2 | 3 | 4 | ...
+def readNumber(q):
+    number = q.dequeue()
+    if int(number) > 1:
+        return
+    raise Syntaxfel("För litet tal")
+
+#-----------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------
+
+#Next, we define a function to store the input molecule as a linked queue:
+
+def storeMolecule(molecule):
+    q = LinkedQ()
+    for letter in molecule:
+        q.enqueue(letter)
+    q.enqueue(None)
+    return q
+
+#-----------------------------------------------------------------------------------------------------------------------------
+#Finally, we define the checkStructure function that will parse the molecule queue using the previously defined functions and return the appropriate message:
+def checkStructure(molecule):
+    q = storeMolecule(molecule)
+
+    try:
+        readMolecule(q)
+        return "Formeln är syntatiskt korrekt"
+    except Syntaxfel as fel:
+        return str(fel)
+
+#-----------------------------------------------------------------------------------------------------------------------------
+#Finally, we define the main function that prompts the user to input a molecule, checks its syntax using checkStructure, and prints the appropriate message:
+def main():
+    molecule = input("Skriv en molekyl: ")
+    result = checkStructure(molecule)
+    print(result)
+#----------------------------------------------------------------------------------------------------------------------------- 
+if __name__ == "__main__":
+    main()
---
+++ Laboratorium_8/linkedQueue.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+    def __str__(self):
+        return str(self.data)
+
+
+class LinkedQ:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def print(self):
+        temp = self.head
+
+        while temp:
+            print(temp.data)
+            temp = temp.next
+
+    def size(self):
+        temp = self.head
+        count = 0
+        while temp:
+            count += 1
+            temp = temp.next
+        return count
+
+    def isEmpty(self):
+        if self.tail != None:
+            return False
+        else:
+            return True
+
+    def enqueue(self, item):
+        newNode = Node(item)
+
+        if self.head == None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = newNode
+            self.tail = newNode
+
+    def peek(self):
+        if self.head == None:
+            return None
+        else:
+            return self.head.data
+
+    def dequeue(self):
+        if self.head == None:
+            return None
+
+        val = self.head.data
+        self.head = self.head.next
+
+        if self.head == None:
+            self.tail = None
+
+        return val
