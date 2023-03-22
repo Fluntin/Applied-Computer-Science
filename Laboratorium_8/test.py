41d06420376e113ed99afb33cee439c48ff87145
Fluntin <112729558+Fluntin@users.noreply.github.com>
2023-03-22 20:07:28 +0100

    123

--- Laboratorium_8/test.py
+++ Laboratorium_8/test.py
@@ -4,15 +4,19 @@ from Syntaxfel import *
 
 
 class SyntaxTest(unittest.TestCase):
+    
+    def testNumber(self):
+        self.assertEqual(checkStructure("Cr0"), "För litet tal vid radslutet")
+        self.assertEqual(checkStructure("Pb1"), "För litet tal vid radslutet")
+        self.assertEqual(checkStructure("H01011"), "För litet tal vid radslutet 1011")
+        self.assertEqual(checkStructure("Cr2"), "Formeln är syntatiskt korrekt")
+        self.assertEqual(checkStructure("Cr1"), "För litet tal vid radslutet")
 
     def testLETTERletter(self):
         self.assertEqual(checkStructure("Cr"), "Formeln är syntatiskt korrekt")
-        self.assertEqual(checkStructure("C"), "Formeln är syntatiskt korrekt")
-        self.assertEqual(checkStructure("cr"), "Saknad stor bokstav")
+        self.assertEqual(checkStructure("cr"), "Saknad stor bokstav vid radslutet cr")
     
-    def testNumber(self):
-        self.assertEqual(checkStructure("Cr2"), "Formeln är syntatiskt korrekt")
-        self.assertEqual(checkStructure("Cr1"), "För litet tal vid radslutet")
+
 
 if __name__ == '__main__':
     unittest.main()
